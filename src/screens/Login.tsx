b5eb9b8b4d2aba9bb04fa2e91a236b80999b3e0c
Jeremy Bradshaw <jeremybradshaw@innovedlearning.co.uk>
2018-02-14 19:02:14 +0000

    Commit 1

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,90 @@
-import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 
 export interface Props { }
 export interface State {
   email: string;
   password: string;
+  touched: object;
+}
+
+export default class Login extends React.Component<Props, State> {
+
+  private formValid: boolean = false;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      touched: {}
+    };
+  }
+
+  handleSubmit() {
+    console.log('handleSubmit');
+    this.setState({
+      touched: { ...this.state.touched, submit: true }
+    });
+    console.log(this.state);
+    if (this.formValid) {
+      console.log('login');
+    }
+  }
+
+  validateForm() {
+    return {
+      email: this.state.email.length === 0,
+      password: this.state.password.length === 0
+    };
+  }
+
+  handleBlur = (field) => () => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true }
+    });
+  }
+
+  render() {
+    const errors = this.validateForm();
+    this.formValid = !Object.keys(errors).some(x => errors[x]);
+    const fieldError = (field, afterTouch = true, afterSubmit = true) => {
+      const btn = 'submit';
+      return errors[field] && ((!afterTouch || this.state.touched[field]) || (!afterSubmit || this.state.touched[btn]));
+    };
+
+    console.log('render', errors);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Coach Competencies</Text>
+        <TextInput
+          style={[styles.textinput, fieldError('email') ? styles.error : '']}
+          placeholder='email'
+          keyboardType='email-address'
+          autoCapitalize='none'
+          onChangeText={(email) => this.setState({email})}
+          onBlur={this.handleBlur('email')}
+          value={this.state.email}
+        />
+        {fieldError('email') && <Text style={styles.errorMessage}>Email must be entered</Text>}
+        <TextInput
+          style={[styles.textinput, fieldError('password') ? styles.error : '']}
+          placeholder='password'
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({password})}
+          onBlur={this.handleBlur('password')}
+          value={this.state.password}
+        />
+        {fieldError('password') && <Text style={styles.errorMessage}>Password must be entered</Text>}
+        <TouchableHighlight style={styles.button}
+          underlayColor='lightsteelblue'
+          onPress={this.handleSubmit.bind(this)}
+        >
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles: any = StyleSheet.create({
@@ -20,69 +100,36 @@ const styles: any = StyleSheet.create({
     color: 'steelblue'
   },
   textinput: {
-    margin: 12,
+    margin: 18,
     fontSize: 18,
     backgroundColor: '#DDDDDD',
     width: 300,
-    padding: 6
+    padding: 6,
+    borderWidth: 1,
+    borderColor: 'steelblue',
+    borderRadius: 4
+  },
+  error: {
+    borderColor: 'red'
+  },
+  errorMessage: {
+    color: 'red',
+    fontSize: 12,
+    width: 300,
+    marginTop: -16,
+    height: 16,
+    marginBottom: 0
   },
   button: {
     alignItems: 'center',
     backgroundColor: 'steelblue',
     padding: 10,
-    margin: 12,
+    margin: 18,
     width: 200,
     borderRadius: 6
   },
   buttonText: {
     color: 'white',
     fontSize: 20
-  },
-  error: {
-    color: 'red'
   }
 });
-
-export default class Login extends React.Component<Props, State> {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
-
-  login() {
-    console.log('login');
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.heading}>Coach Competencies</Text>
-        <TextInput style={styles.textinput}
-          placeholder='email'
-          autoCapitalize='none'
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
-        />
-        <TextInput style={styles.textinput}
-          placeholder='password'
-          secureTextEntry={true}
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-        />
-        <Text style={styles.error}>
-        </Text>
-        <TouchableHighlight style={styles.button}
-          underlayColor='lightsteelblue'
-          onPress={this.login.bind(this)}
-        >
-          <Text style={styles.buttonText}>Log In</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-}

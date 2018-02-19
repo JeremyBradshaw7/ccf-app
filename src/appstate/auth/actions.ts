01694b27a503c1dd2461fe539fa4ee5886fa8ffb
Jeremy Bradshaw <jeremybradshaw@innovedlearning.co.uk>
2018-02-19 12:43:41 +0000

    action type type safety

--- src/appstate/auth/actions.ts
+++ src/appstate/auth/actions.ts
@@ -22,17 +22,55 @@ import Api from '../../services/api';
 //   }
 // });
 
-export enum ActionKey {
+// Used as reference: https://spin.atomicobject.com/2017/07/24/redux-action-pattern-typescript/
+
+// enumeration of all of the action type keys we wish to dispatch on in our application
+export enum AuthActionTypeKeys {
   AUTH_LOGIN = 'AUTH_LOGIN', // starting login
   AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS', // login succeeded
   AUTH_LOGIN_FAIL = 'AUTH_LOGIN_FAIL', // login failed
-  AUTH_LOGOUT = 'AUTH_LOGOUT' // logged out
+  AUTH_LOGOUT = 'AUTH_LOGOUT', // logged out
+  OTHER_ACTION = '__any_other_action_type__'
 }
 
-// But our login will be asynchronous, via the api service layer
+// define types for our specific actions. I think of these more as 'events'
+// these add type safety to our reducers, it knows which action properties are available per type
+export interface ILoginAction {
+  type: AuthActionTypeKeys.AUTH_LOGIN;
+  email: string;
+}
+export interface ILoginSuccessAction {
+  type: AuthActionTypeKeys.AUTH_LOGIN_SUCCESS;
+  user: string;
+  token: string;
+  email: string;
+}
+export interface ILoginFailAction {
+  type: AuthActionTypeKeys.AUTH_LOGIN_FAIL;
+  email: string;
+  error: string;
+}
+export interface ILogoutAction {
+  type: AuthActionTypeKeys.AUTH_LOGOUT;
+}
+// define an OtherAction type (which we never dispatch) that lives in our ActionTypes, so TypeScript will warn us if it’s not handled.
+export interface IOtherAction {
+  type: AuthActionTypeKeys.OTHER_ACTION;
+}
+// create a type representing all our auth actions
+export type IAuthActionTypes =
+    | ILoginSuccessAction
+    | ILoginAction
+    | ILoginFailAction
+    | ILogoutAction
+    | IOtherAction;
+
+// Now our action CREATORS, I think of these more as 'actions'
+// Our login will be asynchronous, via the api service layer
+// Not sure (yet) how to add type safety to the calls to these action creators from the screens
 export const loginUser = (email: string, password: string) => {
   return dispatch => {
-    dispatch({ type: ActionKey.AUTH_LOGIN, payload: email });
+    dispatch({ type: AuthActionTypeKeys.AUTH_LOGIN, payload: email });
     console.log('call Api', email, password);
     Api.Login(email, password)
       .then(response => {
@@ -43,25 +81,20 @@ export const loginUser = (email: string, password: string) => {
         console.log('token', token);
         if (data['status'] === 'success') {
           const user = data.user;
-          // status:200
-          // _bodyInit:
-          // "{"status":"success","id":"a1954...3c852",
-          // "user":{"id":1,"name":"Innoved Administration","role":"Global Administrator",
-          // "privileges":["Access","Create","Style","Update"],"access":["foundation","youth","senior","expert"]}}"
           dispatch({
-            type: ActionKey.AUTH_LOGIN_SUCCESS,
+            type: AuthActionTypeKeys.AUTH_LOGIN_SUCCESS,
             payload: { user: user, token: token, email: email }
           });
         } else {
           dispatch({
-            type: ActionKey.AUTH_LOGIN_FAIL,
+            type: AuthActionTypeKeys.AUTH_LOGIN_FAIL,
             payload: { email: email, error: data['error'] }
           });
         }
       })
       .catch(err => {
         dispatch({
-          type: ActionKey.AUTH_LOGIN_FAIL,
+          type: AuthActionTypeKeys.AUTH_LOGIN_FAIL,
           payload: { email: email, error: err }
         });
         console.log('FAIL!', err);
@@ -70,5 +103,5 @@ export const loginUser = (email: string, password: string) => {
 };
 
 export const logoutUser = () => ({
-  type: ActionKey.AUTH_LOGOUT
+  type: AuthActionTypeKeys.AUTH_LOGOUT
 });
--- src/appstate/auth/reducers.ts
+++ src/appstate/auth/reducers.ts
@@ -1,7 +1,7 @@
 // Authorisation reducers
 // Keep stuff to do with authentication at application state level
 
-import { ActionKey } from './actions';
+import { AuthActionTypeKeys, IAuthActionTypes } from './actions';
 
 // what data do we need to persist in our application state to do with authentication?
 const INITIAL_STATE = {
@@ -16,17 +16,35 @@ const INITIAL_STATE = {
 // what actions from our dumb front end do we want to react to?
 // dont include every email, password text change unless we want to use redux-form for form validation (may do later, but it's not advised
 // We are firing these events back to the forms subscribed to this reducer(?)
-export default (state = INITIAL_STATE, action) => {
+export default (state = INITIAL_STATE, action: IAuthActionTypes) => {
   switch (action.type) {
-    case ActionKey.AUTH_LOGIN: // on log in submitted
-      return { ...state, email: action.payload, isLoggedIn: false, loading: true, error: '', token: ''};
-    case ActionKey.AUTH_LOGIN_SUCCESS: // on completing log in
-      return { ...state,
-        email: action.payload.email, isLoggedIn: true, loading: false,
-        error: '', token: action.payload.token, user: action.payload.user};
-    case ActionKey.AUTH_LOGIN_FAIL: // on failing to log in
-      return { ...state, email: action.payload.email, isLoggedIn: false, loading: false, error: action.payload.error, token: ''};
-    case ActionKey.AUTH_LOGOUT: // on logging out
+    case AuthActionTypeKeys.AUTH_LOGIN: // on log in submitted
+      return {
+        ...state,
+        email: action.email,
+        isLoggedIn: false,
+        loading: true,
+        error: '',
+        token: ''
+      };
+    case AuthActionTypeKeys.AUTH_LOGIN_SUCCESS: // on completing log in
+      return {
+        ...state,
+        email: action.email, isLoggedIn: true, loading: false,
+        error: '',
+        token: action.token,
+        user: action.user
+      };
+    case AuthActionTypeKeys.AUTH_LOGIN_FAIL: // on failing to log in
+      return {
+        ...state,
+        email: action.email,
+        isLoggedIn: false,
+        loading: false,
+        error: action.error,
+        token: ''
+      };
+    case AuthActionTypeKeys.AUTH_LOGOUT: // on logging out, revert to initial state
       return { ...state, ...INITIAL_STATE};
     default:
       return state;
